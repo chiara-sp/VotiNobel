dbe4109bd5beec833b93226cdb43597901bef799
chiaraspirito <chiaraspirito@air-di-chiara.lan>
2021-04-30 11:09:37 +0200

    soluzione professore

--- src/main/java/it/polito/tdp/nobel/model/Model.java
+++ src/main/java/it/polito/tdp/nobel/model/Model.java
@@ -1,16 +1,106 @@
 package it.polito.tdp.nobel.model;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
-public class Model {
+import it.polito.tdp.nobel.db.EsameDAO;
 
+public class Model {
 	
-	public Set<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
-		System.out.println("TODO!");
-		return null;	
+	private List<Esame> partenza; 
+	private Set<Esame> soluzioneMigliore;
+	private double mediaSoluzioneMigliore;
+	
+	public Model() {
+		EsameDAO dao= new EsameDAO();
+		this.partenza= dao.getTuttiEsami();
 	}
 
 	
+	public Set<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
+		Set<Esame> parziale= new HashSet<>();
+		soluzioneMigliore= new HashSet<>();
+		mediaSoluzioneMigliore=0;
+		
+		//cerca(parziale, 0, numeroCrediti);
+		cerca1(parziale, 0, numeroCrediti);
+		
+		return soluzioneMigliore;	
+	}
+
+	//complessita' 2^N
+	private void cerca1(Set<Esame> parziale, int L, int m) {
+		//casi terminali
+		
+				int crediti= sommaCrediti(parziale);
+				
+				if(crediti> m) {
+					return;
+				}
+				if(crediti==m) {
+					double media=calcolaMedia(parziale);
+					if(media> mediaSoluzioneMigliore) {
+						soluzioneMigliore= new HashSet<>(parziale);
+						mediaSoluzioneMigliore= media;
+					}
+					
+					return;
+				}
+				//sicuramente crediti< m 
+				//L= PARTENZA.size() -> non ci sono più esami da aggiungere
+				if(L == partenza.size()) {
+					return;
+				}
+				
+				//generare i sotto-problemi
+				//partenza [L] da aggiungere o no? provo entrambe le cose
+				
+				parziale.add(partenza.get(L));
+				cerca1(parziale,L+1, m);
+				parziale.remove(partenza.get(L));
+				cerca1(parziale, L+1, m);
+				
+		
+	}
+
+
+	//  COMPLESSITA' : N!
+	private void cerca(Set<Esame> parziale, int L, int m) {
+		//casi terminali
+		
+		int crediti= sommaCrediti(parziale);
+		
+		if(crediti> m) {
+			return;
+		}
+		if(crediti==m) {
+			double media=calcolaMedia(parziale);
+			if(media> mediaSoluzioneMigliore) {
+				soluzioneMigliore= new HashSet<>(parziale);
+				mediaSoluzioneMigliore= media;
+			}
+			
+			return;
+		}
+		//sicuramente crediti< m 
+		//L= PARTENZA.size() -> non ci sono più esami da aggiungere
+		if(L == partenza.size()) {
+			return;
+		}
+		
+		//generare i sotto-problemi
+		for(Esame e: partenza) {
+			if(!parziale.contains(e)) {
+				parziale.add(e);
+				cerca(parziale, L+1,m);
+				parziale.remove(e);
+			}
+		}
+		
+	}
+
+
 	public double calcolaMedia(Set<Esame> esami) {
 		
 		int crediti = 0;
